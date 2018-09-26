4a472a2dd6d6618b4b5d7c72e99fb6a76aaa2f4c
Guido Oliveira <guidodutra@gmail.com>
2018-09-26 19:34:08 -0300

    debugging user creation

--- server/src/db/seeds/users.js
+++ server/src/db/seeds/users.js
@@ -4,13 +4,11 @@ exports.seed = knex => knex('user')
   .del()
   .then(() => knex('user').insert([
     {
-      id: 1,
       name: 'guido',
       email: 'guido@email',
       password: bcrypt.hashSync('senha', 10),
     },
     {
-      id: 2,
       name: 'bruno',
       email: 'bruno@email',
       password: bcrypt.hashSync('senha', 10),
--- server/src/graphql/resolvers/mutation/createUser.js
+++ server/src/graphql/resolvers/mutation/createUser.js
@@ -1,3 +1,3 @@
 import User from '../../../data/user';
 
-export default async (_, args) => User.create(args.username, args.password);
+export default async (_, args) => User.create(args);
