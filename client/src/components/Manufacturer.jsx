5ed0e4523b3af4c287f6f333f78e93dd0c5f3663
Guido Oliveira <guidodutra@gmail.com>
2018-12-04 12:03:20 -0200

    added bootstrap dependencies

--- client/src/components/Manufacturer.jsx
+++ client/src/components/Manufacturer.jsx
@@ -21,9 +21,35 @@ const Manufacturer = () => (
 
       return (
         <div>
-          {manufacturer.name}
-          {manufacturer.phone}
+          <div className="jumbotron text-center">
+            <h1>My First Bootstrap Page</h1>
+            <p>Resize this responsive page to see the effect!</p> 
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-4">
+                <h3>Column 1</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
+              <div className="col-sm-4">
+                <h3>Column 2</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
+              <div className="col-sm-4">
+                <h3>Column 3</h3>
+                <p>Lorem ipsum dolor..</p>
+                <p>Ut enim ad..</p>
+              </div>
+            </div>
+          </div>
         </div>
+        // <div>
+        //   {manufacturer.name}
+        //   {manufacturer.phone}
+        // </div>
       );
     }}
   </Query>
